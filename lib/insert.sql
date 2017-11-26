128a32ff5e68172a191184bb44ed63284549e9f3
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-26 02:09:38 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Happy", 1);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Rowdy", 10, "M", "Brown", "Sad", 1);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Tabitha", 7, "M", "Black", "Sad", 0);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Happy", 1);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Melissa", 7, "M", "Brown", "Happy", 0);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Grinch", 10, "M", "Black", "Sad", 1);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES ("Wendy", 7, "M", "Brown", "Happy", 0);
+INSERT INTO bears(name, age, gender, color, temperment, alive) VALUES (NULL, 10, "M", "Black", "Angry", 1);
